32619faa08bae90fa203acc41a41fae975b02a8f
Lee Zamparo <zamparo@gmail.com>
2018-12-19 16:31:41 -0500

    paid down some technical debt, reorganizing DEseq workflow into compute... and figure generation into make figures.  differential peak list script now dead code

--- src/compute_raw_counts_per_condition.R
+++ src/compute_raw_counts_per_condition.R
@@ -65,104 +65,113 @@ flanks_dds <- readRDS("../../results/flank_dds.rds")
 sizeFactors(dds) <- sizeFactors(flanks_dds)
 
 dds_peaks <- DESeq(dds, parallel=TRUE)
-
-# Check that we're getting replicates clustering together
-rld <- rlog(dds_peaks, blind=FALSE)
-
-# PCA of conditions where batch effects are removed
-mat <- assay(rld)
-mat_nobatch <- limma::removeBatchEffect(mat, design=model.matrix(~ col_data$Condition), batch=col_data$Batch, batch2=col_data$Adapter)
-assay(rld) <- mat_nobatch
-data <- plotPCA(rld, intgroup=c("Condition"), returnData=TRUE)  ### pick ntop based on number of up & down peaks.
-percentVar <- round(100 * attr(data, "percentVar"))
-
-# For ASH: need just P-A-SA-SAR conditions...
-ggplot(subset(data, data$Condition %in% c("WT","ASXL1","SRSF2-ASXL1","SRSF2-ASXL1-NRAS")), aes(PC1, PC2, color=Condition)) +
-  geom_point(size=3) +
-  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
-  ylab(paste0("PC2: ",percentVar[2],"% variance")) +
-  coord_fixed() +
-  ggtitle("PCA of log transformed read counts") +
-  geom_text_repel(
-    aes(label = name),
-    size = 3,
-    box.padding = unit(0.35, "lines"),
-    point.padding = unit(0.3, "lines")) 
-
-
-### Get the results for each treatment condition versus WT along the major 
-### expected differentiation path
-
 sig_alpha <- 0.05
+
+
+### Get the results for each treatment condition versus P
 
 # A vs P
 res_A_P <- results(dds_peaks, contrast=c("Condition", "P", "A"), alpha=sig_alpha, independentFiltering=TRUE, parallel=TRUE)
 summary(res_A_P)
-plotMA(res_A_P, main="MA plot for gains, losses in A vs P", ylim=c(-2,2))
 idx <- res_A_P$padj < 0.05
 A_P_diff_peaks <- rownames(res_A_P)[idx]
 
 # SA vs P
 res_SA_P<- results(dds_peaks, contrast=c("Condition", "P", "SA"), alpha=sig_alpha, independentFiltering=TRUE, parallel=TRUE)
 summary(res_SA_P)
-plotMA(res_SA_P, main="MA plot for gains, losses in SA vs P", ylim=c(-2,2))
 idx <- res_SA_P$padj < 0.05
 SA_P_diff_peaks <- rownames(res_SA_P)[idx]
 
 # SAR vs P
 res_SAR_P <- results(dds_peaks, contrast=c("Condition", "P", "SAR"), alpha=sig_alpha, independentFiltering=TRUE, parallel=TRUE)
 summary(res_SAR_P)
-plotMA(res_SAR_P, main="MA plot for gains, losses in SAR vs P", ylim=c(-2,2))
 idx <- res_SAR_P$padj < 0.05
 SAR_P_diff_peaks <- rownames(res_SAR_P)[idx]
 
-### Now look at P vs S, AS vs S, SA vs A, SAR vs SA
+# SARF vs P
+res_SARF_P <- results(dds_peaks, contrast=c("Condition", "P", "SARF"), alpha=sig_alpha, independentFiltering=TRUE, parallel=TRUE)
+summary(res_SARF_P)
+idx <- res_SARF_P$padj < 0.05
+SARF_P_diff_peaks <- rownames(res_SARF_P)[idx]
+
+# SARN vs P
+res_SARN_P <- results(dds_peaks, contrast=c("Condition", "P", "SARN"), alpha=sig_alpha, independentFiltering=TRUE, parallel=TRUE)
+summary(res_SARN_P)
+idx <- res_SARN_P$padj < 0.05
+SARN_P_diff_peaks <- rownames(res_SARN_P)[idx]
+
+### Now look at S vs P, SA vs S, SA vs A, SAR vs SA
 res_S_P <- results(dds_peaks, contrast=c("Condition", "P", "S"), alpha=sig_alpha, independentFiltering=TRUE, parallel=TRUE)
 summary(res_S_P)
-plotMA(res_S_P, main="MA plot for gains, losses in S vs P", ylim=c(-2,2))
 idx <- res_S_P$padj < 0.05
 S_P_diff_peaks <- rownames(res_S_P)[idx]
 
 res_SA_S <- results(dds_peaks, contrast=c("Condition", "S", "SA"), alpha=sig_alpha, independentFiltering=TRUE, parallel=TRUE)
 summary(res_SA_S)
-plotMA(res_SA_S, main="MA plot for gains, losses in SA vs S", ylim=c(-2,2))
 idx <- res_SA_S$padj < 0.05
 SA_S_diff_peaks <- rownames(res_SA_S)[idx]
 
 res_SA_A <- results(dds_peaks, contrast=c("Condition", "A", "SA"), alpha=sig_alpha, independentFiltering=TRUE, parallel=TRUE)
 summary(res_SA_A)
-plotMA(res_SA_A, main="MA plot for gains, losses in SA vs A", ylim=c(-2,2))
 idx <- res_SA_A$padj < 0.05
 SA_A_diff_peaks <- rownames(res_SA_A)[idx]
 
 res_SAR_SA <- results(dds_peaks, contrast=c("Condition", "SA", "SAR"), alpha=sig_alpha, independentFiltering=TRUE, parallel=TRUE)
 summary(res_SAR_SA)
-plotMA(res_SAR_SA, main="MA plot for gains, losses in SAR vs SA", ylim=c(-2,2))
 idx <- res_SAR_SA$padj < 0.05
 SAR_SA_diff_peaks <- rownames(res_SAR_SA)[idx]
 
-setwd('../results/DESeq')
+# Include SARN & SARF in the comparisons
+# SARN vs SAR
+res_SARN_SAR <- results(dds_peaks, contrast=c("Condition", "SARN", "SAR"), alpha=sig_alpha, independentFiltering=TRUE, parallel=TRUE)
+summary(res_SARN_SAR)
+idx <- res_SARN_SAR$padj < 0.05
+SARN_SAR_diff_peaks <- rownames(res_SARN_SAR)[idx]
 
-# collect those list of peaks that are differential among the P-A, A-SA, SA-SAR pairwise comparisons
-union_diff_peaks_main_trajectory <- union(union(A_WT_diff_peaks, SA_A_diff_peaks), SA_SAR_diff_peaks)
+# SARF vs SAR
+res_SARF_SAR <- results(dds_peaks, contrast=c("Condition", "SARF", "SAR"), alpha=sig_alpha, independentFiltering=TRUE, parallel=TRUE)
+summary(res_SARF_SAR)
+idx <- res_SARF_SAR$padj < 0.05
+SARF_SAR_diff_peaks <- rownames(res_SARF_SAR)[idx]
+
+setwd('../../results/DESeq')
+
+# collect those list of peaks that are differential among the A-PA, SA-A, SAR-SA pairwise comparisons
+union_diff_peaks_main_trajectory <- union(union(A_P_diff_peaks, SA_A_diff_peaks), SAR_SA_diff_peaks)
 bed_matrix <- str_split_fixed(union_diff_peaks_main_trajectory, "-", 3)
 bed_df <- as.data.frame(bed_matrix)
-write.table(bed_df, "peak_lists/WT_A_SA_SAR_diff_peaks.bed", sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
+write.table(bed_df, "../peaks/P_A_SA_SAR_diff_peaks.bed", sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
 
-# collect those list of peaks that are differential among the P-A, P-SA, P-SAR pairwise comparisons
-union_diff_peaks_conditions_vs_WT <- union(union(A_WT_diff_peaks, SA_WT_diff_peaks), SAR_WT_diff_peaks)
-bed_matrix <- str_split_fixed(union_diff_peaks_conditions_vs_WT, "-", 3)
+# collect those list of peaks that are differential among the A-P, SA-P, SAR-P, SARN-P, SARF-P pairwise comparisons
+union_diff_peaks_conditions_vs_P <- union(union(A_P_diff_peaks, SA_P_diff_peaks), union(union(SAR_P_diff_peaks, SARF_P_diff_peaks),SARN_P_diff_peaks))
+bed_matrix <- str_split_fixed(union_diff_peaks_conditions_vs_P, "-", 3)
 bed_df <- as.data.frame(bed_matrix)
-write.table(bed_df, "peak_lists/all_conditions_vs_WT_diff_peaks.bed", sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
+write.table(bed_df, "../peaks/all_conditions_vs_P_diff_peaks.bed", sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
+
+# collect those list of peaks that are differential among the A-P, SA-A, SAR-SA, SARN-SAR, SARF-SAR pairwise comparisons
+union_diff_peaks_A_first <- union(union(A_P_diff_peaks, SA_A_diff_peaks), union(union(SAR_SA_diff_peaks, SARN_SAR_diff_peaks), union(SARF_SAR_diff_peaks, SAR_SA_diff_peaks)))
+bed_matrix <- str_split_fixed(union_diff_peaks_A_first, "-", 3)
+bed_df <- as.data.frame(bed_matrix)
+write.table(bed_df, "../peaks/P_A_SA_SAR_SARN_SARF_diff_peaks.bed", sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
+
+# collect those list of peaks that are differential among the S-P, SA-S, SAR-SA, SARN-SAR, SARF-SAR pairwise comparisons
+union_diff_peaks_S_first <- union(union(S_P_diff_peaks, SA_S_diff_peaks), union(union(SAR_SA_diff_peaks, SARN_SAR_diff_peaks), union(SARF_SAR_diff_peaks, SAR_SA_diff_peaks)))
+bed_matrix <- str_split_fixed(union_diff_peaks_S_first, "-", 3)
+bed_df <- as.data.frame(bed_matrix)
+write.table(bed_df, "../peaks/P_S_SA_SAR_SARN_SARF_diff_peaks.bed", sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
 
 # save this set, plus the results objets for later retrieval.  
-setwd('../results/DESeq')
+setwd('./objects/')
 saveRDS(dds_peaks, file = "dds_object.rds")
-saveRDS(res_A_WT, file = "res_A_WT.rds")
-saveRDS(res_S_WT, file = "res_S_WT.rds")
-saveRDS(res_SA_WT, file = "res_SA_WT.rds")
-saveRDS(res_SAR_WT, file = "res_SAR_WT.rds")
+saveRDS(res_A_P, file = "res_A_P.rds")
+saveRDS(res_S_P, file = "res_S_P.rds")
+saveRDS(res_SA_P, file = "res_SA_P.rds")
+saveRDS(res_SAR_P, file = "res_SAR_P.rds")
+saveRDS(res_SARF_P, file = "res_SARF_P.rds")
+saveRDS(res_SARN_P, file = "res_SARN_P.rds")
 
 saveRDS(res_SA_A, file = "res_SA_A.rds")
 saveRDS(res_SA_S, file = "res_SA_S.rds")
-saveRDS(res_SA_SAR, file = "res_SA_SAR.rds")
+saveRDS(res_SAR_SA, file = "res_SAR_SA.rds")
+saveRDS(res_SARN_SAR, file = "res_SARN_SAR.rds")
+saveRDS(res_SARF_SAR, file = "res_SARF_SAR.rds")
--- src/make_figures.R
+++ src/make_figures.R
@@ -7,7 +7,7 @@ require(cowplot)
 # plot pie chart of atlas, differentiable peaks atlas
 
 # load atlas file
-setwd("/Users/zamparol/projects/AML_ATAC/peaks")
+setwd("~/projects/AML_ATAC/peaks")
 atlas = read.csv("annotated_atlas_reduced.csv")
 atlas_dt = data.table(atlas)
 setnames(atlas_dt, "seqnames", "chrom")
@@ -31,8 +31,7 @@ atlas_pie = ggplot(atlas_dt, aes(x=factor(1), fill=annotation)) +
   geom_text_repel(data=grouped_peaks, aes(x=factor(1), y=pos, label=percent(percentage)), vjust= 1.5, size=6)
 
 # pie chart of diff'ble peaks
-setwd("/Users/zamparol/projects/AML_ATAC/results/DESeq/peak_lists")
-diff_peaks = read.delim(file="WT_A_SA_SAR_diff_peaks.bed", sep="\t")
+diff_peaks = read.delim(file="P_A_SA_SAR_diff_peaks.bed", sep="\t")
 colnames(diff_peaks) = c("chrom", "start", "end")
 diff_peaks_dt = data.table(diff_peaks)
 key_cols = c("chrom", "start", "end")
@@ -62,50 +61,105 @@ diff_pie = ggplot(annotated_diff_peaks, aes(x=factor(1), fill=annotation)) +
   geom_text_repel(data=grouped_diff_peaks, aes(x=factor(1), y=pos, label=percent(percentage)), size=6)
 
 
-# Bar chart of opening / closing peaks in each transition (P -> A -> SA -> SAR)
+# Load the DESeq objects used to make differential plots
 setwd('/Users/zamparol/projects/AML_ATAC/results/DESeq/objects')
 dds_peaks = readRDS("dds_object.rds")
-res_WT_A = readRDS("res_A_WT.rds")
-res_A_SA = readRDS("res_SA_A.rds")
-res_SA_SAR = readRDS("res_SA_SAR.rds")
-res_WT_S = readRDS("res_S_WT.rds")
-res_S_SA = readRDS("res_SA_S.rds")
+res_A_P = readRDS("res_A_P.rds")
+res_SA_A = readRDS("res_SA_A.rds")
+res_SAR_SA = readRDS("res_SAR_SA.rds")
+res_S_P = readRDS("res_S_P.rds")
+res_SA_S = readRDS("res_SA_S.rds")
+res_SARF_SAR = readRDS("res_SARF_SAR.rds")
+res_SARN_SAR = readRDS("res_SARN_SAR.rds")
 
-res_WT_A_dt = as.data.table(res_WT_A)
-res_WT_S_dt = as.data.table(res_WT_S)
-res_A_SA_dt = as.data.table(res_A_SA)
-res_S_SA_dt = as.data.table(res_S_SA)
-res_SA_SAR_dt = as.data.table(res_SA_SAR)
+res_A_P_dt = as.data.table(res_A_P)
+res_S_P_dt = as.data.table(res_S_P)
+res_SA_A_dt = as.data.table(res_SA_A)
+res_SA_S_dt = as.data.table(res_SA_S)
+res_SAR_SA_dt = as.data.table(res_SAR_SA)
+res_SARN_SAR_dt = as.data.table(res_SARN_SAR)
+res_SARF_SAR_dt = as.data.table(res_SARF_SAR)
 
-peaks_up = c(res_WT_A_dt[padj < 0.05 & log2FoldChange > 0, .N ], res_WT_S_dt[padj < 0.05 & log2FoldChange > 0, .N ], res_A_SA_dt[padj < 0.05 & log2FoldChange > 0, .N ], res_S_SA_dt[padj < 0.05 & log2FoldChange > 0, .N ], res_SA_SAR_dt[padj < 0.05 & log2FoldChange > 0, .N ])
-peaks_down = c(res_WT_A_dt[padj < 0.05 & log2FoldChange < 0, .N ], res_WT_S_dt[padj < 0.05 & log2FoldChange < 0, .N ], res_A_SA_dt[padj < 0.05 & log2FoldChange < 0, .N ], res_S_SA_dt[padj < 0.05 & log2FoldChange < 0, .N ], res_SA_SAR_dt[padj < 0.05 & log2FoldChange < 0, .N ])
-conditions = rep(c("A vs P", "S vs P", "SA vs A", "SA vs S", "SAR vs SA"),2)
+### PCA plot
+# Check that we're getting replicates clustering together
+rld <- rlog(dds_peaks, blind=FALSE)
+
+# PCA of conditions where batch effects are removed
+mat <- assay(rld)
+mat_nobatch <- limma::removeBatchEffect(mat, design=model.matrix(~ col_data$Condition), batch=col_data$Batch, batch2=col_data$Adapter)
+assay(rld) <- mat_nobatch
+data <- plotPCA(rld, intgroup=c("Condition"), returnData=TRUE)  ### pick ntop based on number of up & down peaks.
+percentVar <- round(100 * attr(data, "percentVar"))
+
+# For ASH: need just P-A-SA-SAR conditions...
+ggplot(subset(data, data$Condition %in% c("WT","ASXL1","SRSF2-ASXL1","SRSF2-ASXL1-NRAS")), aes(PC1, PC2, color=Condition)) +
+  geom_point(size=3) +
+  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+  ylab(paste0("PC2: ",percentVar[2],"% variance")) +
+  coord_fixed() +
+  ggtitle("PCA of log transformed read counts") +
+  geom_text_repel(
+    aes(label = name),
+    size = 3,
+    box.padding = unit(0.35, "lines"),
+    point.padding = unit(0.3, "lines")) 
+
+
+# MA plots versus P
+plotMA(res_A_P, main="MA plot for gains, losses in A vs P", ylim=c(-2,2))
+plotMA(res_S_P, main="MA plot for gains, losses in S vs P", ylim=c(-2,2))
+plotMA(res_SA_P, main="MA plot for gains, losses in SA vs P", ylim=c(-2,2))
+plotMA(res_SAR_P, main="MA plot for gains, losses in SAR vs P", ylim=c(-2,2))
+plotMA(res_SARF_P, main="MA plot for gains, losses in SARF vs P", ylim=c(-2,2))
+plotMA(res_SARN_P, main="MA plot for gains, losses in SARN vs P", ylim=c(-2,2))
+
+# Remaining MA plots in (P -> A / S -> SA -> SAR -> SARN / SARF)
+plotMA(res_SA_S, main="MA plot for gains, losses in SA vs S", ylim=c(-2,2))
+plotMA(res_SA_A, main="MA plot for gains, losses in SA vs A", ylim=c(-2,2))
+plotMA(res_SAR_SA, main="MA plot for gains, losses in SAR vs SA", ylim=c(-2,2))
+plotMA(res_SARN_SAR, main="MA plot for gains, losses in SARN vs SAR", ylim=c(-2,2))
+plotMA(res_SARF_SAR, main="MA plot for gains, losses in SARF vs SAR", ylim=c(-2,2))
+
+# Bar chart of opening / closing peaks in each transition 
+# (P -> A -> SA -> SAR-> SARN / SARF)
+peaks_up = c(res_A_P_dt[padj < 0.05 & log2FoldChange > 0, .N ], res_SA_A_dt[padj < 0.05 & log2FoldChange > 0, .N ], res_SAR_SA_dt[padj < 0.05 & log2FoldChange > 0, .N ], res_SARN_SAR_dt[padj < 0.05 & log2FoldChange > 0, .N ], res_SARF_SAR_dt[padj < 0.05 & log2FoldChange > 0, .N ])
+peaks_down = c(res_A_P_dt[padj < 0.05 & log2FoldChange < 0, .N ], res_SA_A_dt[padj < 0.05 & log2FoldChange < 0, .N ], res_SAR_SA_dt[padj < 0.05 & log2FoldChange < 0, .N ], res_SARN_SAR_dt[padj < 0.05 & log2FoldChange < 0, .N ], res_SARF_SAR_dt[padj < 0.05 & log2FoldChange < 0, .N ])
+conditions = rep(c("A vs P", "SA vs A", "SAR vs SA", "SARN vs SAR", "SARF vs SAR"),2)
 amount = c(peaks_up, peaks_down)
 change = c(rep("gained", 5),rep("lost",5))
-df = data.frame(conditions, change, amount)
-colnames(df) = c("condition", "change", "amount")
-# correct the factor levels
-df$condition = ordered(rep(c("A vs P", "S vs P", "SA vs A", "SA vs S", "SAR vs SA"),2), levels = c("A vs P", "S vs P", "SA vs A", "SA vs S", "SAR vs SA"))
-rm(list =c("conditions", "amount", "change", "peaks_down", "peaks_up"))
-df$path = c("ASXL truncation first", "SRSF2 P95L first", "ASXL truncation first", "SRSF2 P95L first", "common", "ASXL truncation first", "SRSF2 P95L first", "ASXL truncation first", "SRSF2 P95L first", "common")
+df_A = data.frame(conditions, change, amount)
+df_A$path = "ASXL truncation first"
+colnames(df_A) = c("condition", "change", "amount", "path")
 
-peak_changes = ggplot(df, aes(x=condition, fill=change)) + 
-  geom_bar(data = subset(df, change == "gained" & path %in% c("ASXL truncation first", "common")), aes(y = amount), position="stack", stat="identity") + 
-  geom_bar(data = subset(df, change == "lost" & path %in% c("ASXL truncation first", "common")), aes(y = -amount), position="stack", stat="identity") +
-  ggtitle("Peaks gained and lost in P-A-SA-SAR transitions") + ylab("Peaks") + xlab("Transition") + 
+# (P -> S -> SA -> SAR-> SARN / SARF)
+peaks_up = c(res_S_P_dt[padj < 0.05 & log2FoldChange > 0, .N ], res_SA_S_dt[padj < 0.05 & log2FoldChange > 0, .N ], res_SAR_SA_dt[padj < 0.05 & log2FoldChange > 0, .N ], res_SARN_SAR_dt[padj < 0.05 & log2FoldChange > 0, .N ], res_SARF_SAR_dt[padj < 0.05 & log2FoldChange > 0, .N ])
+peaks_down = c(res_S_P_dt[padj < 0.05 & log2FoldChange < 0, .N ], res_SA_S_dt[padj < 0.05 & log2FoldChange < 0, .N ], res_SAR_SA_dt[padj < 0.05 & log2FoldChange < 0, .N ], res_SARN_SAR_dt[padj < 0.05 & log2FoldChange < 0, .N ], res_SARF_SAR_dt[padj < 0.05 & log2FoldChange < 0, .N ])
+conditions = rep(c("S vs P", "SA vs S", "SAR vs SA", "SARN vs SAR", "SARF vs SAR"),2)
+amount = c(peaks_up, peaks_down)
+change = c(rep("gained", 5),rep("lost",5))
+df_S = data.frame(conditions, change, amount)
+df_S$path = "SRSF2 P95L first"
+colnames(df_S) = c("condition", "change", "amount", "path")
+
+# correct the factor levels
+rm(list =c("conditions", "amount", "change", "peaks_down", "peaks_up"))
+
+peak_changes_A = ggplot(df_A, aes(x=condition, fill=change)) + 
+  geom_bar(data = subset(df_A, change == "gained"), aes(y = amount), position="stack", stat="identity") + 
+  geom_bar(data = subset(df_A, change == "lost"), aes(y = -amount), position="stack", stat="identity") +
+  ggtitle("Peaks gained and lost in transitions", subtitle="ASXL truncation first") + ylab("Peaks") + xlab("Transition") + 
   scale_fill_manual(name = "Change", values=c("red", "blue"))
 
-peak_changes_alt_path = ggplot(df, aes(x=condition, fill=change)) + 
-  geom_bar(data = subset(df, change == "gained" & path %in% c("SRSF2 P95L first", "common")), aes(y = amount), position="stack", stat="identity") + 
-  geom_bar(data = subset(df, change == "lost" & path %in% c("SRSF2 P95L first", "common")), aes(y = -amount), position="stack", stat="identity") +
-  ggtitle("Peaks gained and lost in P-S-SA-SAR transitions") + ylab("Peaks") + xlab("Transition") + 
+peak_changes_S = ggplot(df_S, aes(x=condition, fill=change)) + 
+  geom_bar(data = subset(df_S, change == "gained"), aes(y = amount), position="stack", stat="identity") + 
+  geom_bar(data = subset(df_S, change == "lost"), aes(y = -amount), position="stack", stat="identity") +
+  ggtitle("Peaks gained and lost in transitions", subtitle="SRSF2 P95L first") + ylab("Peaks") + xlab("Transition") + 
   scale_fill_manual(name = "Change", values=c("red", "blue"))
 
 # What is up with the lack of changes for A to SA?
 A_SA_hist = ggplot(as.data.frame(res_A_SA), aes(x=pvalue)) + geom_histogram(bins = 100) + xlab("P values") + ggtitle("P values for log2 fold change test for A -> SA transition")
 
 # Load the clustered peaks from plotHeatmap, look at the annotation breakdown, and the genes represented
-setwd("~/projects/AML_ATAC/peaks")
 annotated_atlas <- read.csv(file="annotated_atlas_reduced.csv")
 atlas_dt = data.table(annotated_atlas)
 diff_peaks = read.delim(file="P-A-SA-SAR_clusters.bed", sep="\t", header=TRUE)
