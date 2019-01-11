1d73b0260f9afd298f8027f49722ad3cbbc21364
Lee Zamparo <zamparo@gmail.com>
2019-01-11 14:57:37 -0500

    change to compute matrix, plotHeatmap bw list

--- src/merged_bws_to_tornado.sh
+++ src/merged_bws_to_tornado.sh
@@ -1,14 +1,21 @@
-# run from cpumulticore to complete tornado plots
 #!/bin/bash
 
-condition="P_S_SA_SAR_SARN_SARF"
-bedfile=$(echo "/data/leslie/zamparol/AML_ATAC/data/results/differential_peak_lists/$condition"_diff_peaks.bed)
+# run from cpumulticore to complete tornado plots
+
+resultsdir="/data/leslie/zamparol/AML_ATAC/data/results/"
+condition="SAR_vs_P"
+bedfilesdir="/data/leslie/zamparol/AML_ATAC/data/tornado_cluster_bedfiles/$condition"
 
 matrix_name=$(echo "$condition"_matrix.gz)
-outfile=$(echo "../../results/$condition"_tornado_plot.png)
-title=$(echo "Differentially Accessible Peaks Along $condition")
+outfile_png=$(echo "$resultsdir/figures/$condition"_tornado_plot.png)
+title=$(echo "Differentially Accessible Peaks for $condition")
 
-
-bash compute_matrix.sh -b /data/leslie/zamparol/AML_ATAC -t tracks/deepTools -o $condition -r $bedfile -m matrices/$condition -p 7
-cd /data/leslie/zamparol/AML_ATAC/matrices/$condition
-plotHeatmap -m $matrix_name -o $outfile --kmeans 4 --whatToShow "heatmap and colorbar" -T $title --verbose --refPointLabel "Peak center" --outFileSortedRegions SortedOut_test.bed
+echo "Computing matrix for plotting..."
+pushd ~/projects/tools/atac-atlas/tools
+bash compute_matrix_multibed.sh -b /data/leslie/zamparol/AML_ATAC -t tracks/deepTools -o $condition -r $bedfilesdir -m matrices/$condition -p 7
+popd
+echo "Done."
+echo "Plotting heatmap..."
+cd /data/leslie/zamparol/AML_ATAC/data/matrices/$condition
+plotHeatmap -m $matrix_name -o $outfile_png --whatToShow "heatmap and colorbar" --plotTitle "$title" --verbose --refPointLabel "Peak center"
+echo "Done."
